d12684730ec332f81ee99feaa270f2d957c11cd2
huazai <who.seek.me@java98k.vip>
2019-07-16 11:02:56 +0800

    抽象FastDFS客户端工具类/测试工具类是否可用

---
+++ src/main/java/com/huazai/b2c/aiyou/web/util/FastDFSClient.java
@@ -0,0 +1,118 @@
+package com.huazai.b2c.aiyou.web.util;
+
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient1;
+import org.csource.fastdfs.StorageServer;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description FastDFS Java 客户端工具类
+ *              </ul>
+ * @className FastDFSClient
+ * @package com.huazai.b2c.aiyou.web.util
+ * @createdTime 2017年06月11日
+ *
+ * @version V1.0.0
+ */
+public class FastDFSClient
+{
+
+	private TrackerClient trackerClient = null;
+	private TrackerServer trackerServer = null;
+	private StorageServer storageServer = null;
+	private StorageClient1 storageClient = null;
+
+	public FastDFSClient(String conf) throws Exception
+	{
+		// 1、加载配置文件
+		if (conf.contains("classpath:"))
+		{
+			conf = conf.replace("classpath:", this.getClass().getResource("/").getPath());
+		}
+		// 2、初始化配置
+		ClientGlobal.init(conf);
+		// 3、创建trackerClient对象
+		trackerClient = new TrackerClient();
+		// 4、创建连接并获得trackerServer对象
+		trackerServer = trackerClient.getConnection();
+		// 5、创建一个storageServer的引用
+		storageServer = null;
+		// 6、创建一个storageClient对象
+		storageClient = new StorageClient1(trackerServer, storageServer);
+	}
+
+	/**
+	 * 上传文件方法
+	 * <p>
+	 * Title: uploadFile
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @param fileName
+	 *            文件全路径
+	 * @param extName
+	 *            文件扩展名，不包含（.）
+	 * @param metas
+	 *            文件扩展信息
+	 * @return
+	 * @throws Exception
+	 */
+	public String uploadFile(String fileName, String extName, NameValuePair[] metas) throws Exception
+	{
+		String result = storageClient.upload_file1(fileName, extName, metas);
+		return result;
+	}
+
+	public String uploadFile(String fileName) throws Exception
+	{
+		return uploadFile(fileName, null, null);
+	}
+
+	public String uploadFile(String fileName, String extName) throws Exception
+	{
+		return uploadFile(fileName, extName, null);
+	}
+
+	/**
+	 * 上传文件方法
+	 * <p>
+	 * Title: uploadFile
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @param fileContent
+	 *            文件的内容，字节数组
+	 * @param extName
+	 *            文件扩展名
+	 * @param metas
+	 *            文件扩展信息
+	 * @return
+	 * @throws Exception
+	 */
+	public String uploadFile(byte[] fileContent, String extName, NameValuePair[] metas) throws Exception
+	{
+
+		String result = storageClient.upload_file1(fileContent, extName, metas);
+		return result;
+	}
+
+	public String uploadFile(byte[] fileContent) throws Exception
+	{
+		return uploadFile(fileContent, null, null);
+	}
+
+	public String uploadFile(byte[] fileContent, String extName) throws Exception
+	{
+		return uploadFile(fileContent, extName, null);
+	}
+}
--- src/test/java/com/huazai/b2c/aiyou/fdfs/TFastDFS.java
+++ src/test/java/com/huazai/b2c/aiyou/fdfs/TFastDFS.java
@@ -10,6 +10,8 @@ import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 
+import com.huazai.b2c.aiyou.web.util.FastDFSClient;
+
 /**
  * 
  * @author HuaZai
@@ -26,11 +28,28 @@ import org.junit.Test;
 public class TFastDFS
 {
 
+	private final String file_config = "src/main/resources/resources/fdfs_client.conf";
+
+	/**
+	 * 
+	 * @author HuaZai
+	 * @contact who.seek.me@java98k.vip
+	 * @title TFileUpload
+	 *        <ul>
+	 * @description 测试FastDFS文件服务器是否可用
+	 *              </ul>
+	 * @createdTime 2017年06月11日
+	 * @throws IOException
+	 * @throws MyException
+	 * @return void
+	 *
+	 * @version : V1.0.0
+	 */
 	@Test
 	public void TFileUpload() throws IOException, MyException
 	{
 		// 1、加载配置文件 /aiyou-manager-web/src/main/resources/resources/fdfs_client.conf
-		ClientGlobal.init("src/main/resources/resources/fdfs_client.conf");
+		ClientGlobal.init(file_config);
 		// 2、创建一个TrackerClient对象
 		TrackerClient trackerClient = new TrackerClient();
 		// 3、通过TrackerClient创建与TrackerServer的连接
@@ -42,10 +61,29 @@ public class TFastDFS
 		// 6、上传图片，注意后缀名不需要 . 符号
 		String[] file = storageClient.upload_file("D:\\timg2.jpg", "jpg", null);
 		// 7、打印文件路径
-		for (String string : file)
-		{
-			System.out.println(string);
-		}
+		System.out.println(file);
 
+	}
+
+	/**
+	 * 
+	 * @author HuaZai
+	 * @contact who.seek.me@java98k.vip
+	 * @title TFastDFSConfig
+	 *        <ul>
+	 * @description 测试提取封装的工具类是否可用
+	 *              </ul>
+	 * @createdTime 2017年06月11日
+	 * @return void
+	 *
+	 * @version : V1.0.0
+	 * @throws Exception
+	 */
+	@Test
+	public void TFastDFSConfig() throws Exception
+	{
+		FastDFSClient dfsClient = new FastDFSClient(file_config);
+		String file = dfsClient.uploadFile("D:\\timg2.jpg");
+		System.out.println(file);
 	}
 }
