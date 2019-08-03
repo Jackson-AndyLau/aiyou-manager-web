aba2180df7424ca67b21f3b6c12e5686d5f881fd
huazai <who.seek.me@java98k.vip>
2019-08-03 08:57:32 +0800

    新增搜索controller

---
+++ src/main/java/com/huazai/b2c/aiyou/controller/SearchItemController.java
@@ -0,0 +1,41 @@
+package com.huazai.b2c.aiyou.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.huazai.b2c.aiyou.repo.AiyouResultData;
+import com.huazai.b2c.aiyou.service.SearchItemService;
+
+/**
+ * 
+ * @author HuaZai
+ * @contact who.seek.me@java98k.vip
+ *          <ul>
+ * @description 搜索商品Controller层
+ *              </ul>
+ * @className SearchItemController
+ * @package com.huazai.b2c.aiyou.controller
+ * @createdTime 2017年06月15日
+ *
+ * @version V1.0.0
+ */
+@Controller
+@RequestMapping(value = "/index")
+public class SearchItemController
+{
+
+	@Autowired
+	private SearchItemService searchItemService;
+
+	@RequestMapping(value = "/importAll", method = RequestMethod.POST)
+	@ResponseBody
+	public AiyouResultData importItemAll()
+	{
+		AiyouResultData resultData = searchItemService.importItemAll();
+		return resultData;
+	}
+
+}
